f28163d6a092ee9c4dd9e6f6c76490ac3f6e4c3b
Vincent Maximo II <projects.manager@xdomain.it>
2024-08-06 18:50:15 +0200

    set var user_id, added failure login button

--- popup.js
+++ popup.js
@@ -1,68 +1,72 @@
+const _url = 'https://open-favs.vercel.app'
+const sessionAuthUrl = _url + '/api/v2/auth/signin'
+
+let user_id = null
+
 document.querySelector('#saveButton').addEventListener('click', () => { 
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const activeTab = tabs[0]
     const url = activeTab.url
-    const _url = `https://bookmarks-list.netlify.app`
-
-    //console.log('Fetching data for URL:', url)
+    
 
     /* @@@ check auth @@@ */
 
     chrome.cookies.getAll({ url: _url }, function (cookies) {
-      console.log('into coockies fn')
+      console.log('into cookies fn')
       let isAuthenticated = false
       for (let i = 0; i < cookies.length; i++) {
         if (cookies[i].name === 'sb-access-token') {
           isAuthenticated = true
-          break
+          break;
         }
       }
-      if (isAuthenticated) {
-        alert('autenticated')
+      if (isAuthenticated) {      
+        fetch(sessionAuthUrl, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(response => response.json())
+          .then(data => {
+            user_id = data.session.user.id
+            console.log('Success:', user_id)
+            alert(`Authenticated as user: ${user_id}`)
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+            alert('Failed to get user info')
+          })
       } else {
-        alert('not autenticated')
+        //alert('Not authenticated')
+        document.body.innerHTML = `
+          <h3>Save Current Site</h3>
+          <p>You need to be authenticated to use OpenFavs.</p>
+          <button id="loginButton">Login</button>
+        `
+        document.querySelector('#loginButton').addEventListener('click', () => {
+          window.open('https://open-favs.vercel.app/login/signin', '_blank')
+        })
       }
     })
-
-
-    /* @@@ fetch data @@@ */
-    const apiUrl = `https://bookmarks-list.netlify.app/api/v1/test`
-    const sessionAuthUrl = 'https://open-favs.vercel.app/api/v2/auth/signin'
-
-
-    // Esegui una richiesta fetch al tuo endpoint
-    fetch(apiUrl, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Success:', data);
-        alert(data.message)
-      })
-      .catch((error) => {
-        console.error('Error:', error)
-        alert('Failed to fetch data.')
-      })
-    
-    fetch(sessionAuthUrl, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Success:', data.session.user.id);
-        alert(data.session.user.id)
-      })
-      .catch((error) => {
-        console.error('Error:', error)
-        alert('Failed to fetch data.')
-      })
-
-
   })
 })
+
+// Esegui una richiesta fetch al tuo endpoint
+/*
+const apiUrl = `https://bookmarks-list.netlify.app/api/v1/test`
+  fetch(apiUrl, {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+    alert(data.message)
+  })
+    .catch((error) => {
+      console.error('Error:', error)
+      alert('Failed to fetch data.')
+    })*/
